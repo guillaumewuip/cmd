ebc0e5fce69712dc3d0ecc2d562e357c55e05eb7
Guillaume Clochard <guillaume.clochard@blablacar.com>
2021-06-18 19:29:50 +0200

    cmd-1

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,13 @@ import '../styles/globals.scss'
 
 import { MDXProvider } from '@mdx-js/react'
 
-import { H1, H2, Link, Paragraph, Code } from '../components/Text'
+import { H1, H2, H3, Link, Paragraph, Code } from '../components/Text'
 import { Hr } from '../components/Hr'
 
 const mdComponents = {
   h1: props => <H1 {...props} />,
   h2: props => <H2 {...props} />,
+  h3: props => <H3 {...props} />,
   a: props => <Link {...props} />,
   p: props => <Paragraph {...props} />,
   hr: () => <Hr  />,
