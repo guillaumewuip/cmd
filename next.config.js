727012e9659e26385f0113783483ffc57a78197c
Guillaume Clochard <guillaume.clochard@blablacar.com>
2021-08-21 20:12:36 +0200

    Update libs

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'tsx', 'mdx'],
-  future: {
-    webpack5: true,
-  },
   i18n: {
     locales: ["fr-FR"],
     defaultLocale: "fr-FR",
