f8c6335a80f2201829d0c2e652c4a1e7e2c44126
Guillaume Clochard <guillaume.clochard@blablacar.com>
2021-12-25 17:25:07 +0100

    add api-extractor

--- ui/tokens/src/index.ts
+++ ui/tokens/src/index.ts
@@ -1,5 +1,13 @@
-export * as breakpoints from './breakpoints'
-export * as colors from './colors'
-export * as fonts from './fonts'
-export * as mediaQueries from './media-queries'
-export * as sizes from './sizes'
+import * as breakpoints from './breakpoints'
+import * as colors from './colors'
+import * as fonts from './fonts'
+import * as mediaQueries from './media-queries'
+import * as sizes from './sizes'
+
+export {
+  breakpoints,
+  colors,
+  fonts,
+  mediaQueries,
+  sizes,
+}
