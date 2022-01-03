d550ab15125f84ff428e6f2ae640123937214cf7
Guillaume Clochard <guillaume@wuips.com>
2022-01-03 11:26:26 +0100

    Use https for guillaume.wuips.com

--- ui/footer/src/Footer.tsx
+++ ui/footer/src/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <Small>
-        <Link href="/">cerfeuil et musique douce</Link> - <Link href="/rss/feed.xml">RSS</Link> - <Link href="http://guillaume.wuips.com">Contact</Link> - <Link href="https://github.com/guillaumewuip/cmd">Github</Link>
+        <Link href="/">cerfeuil et musique douce</Link> - <Link href="/rss/feed.xml">RSS</Link> - <Link href="https://guillaume.wuips.com">Contact</Link> - <Link href="https://github.com/guillaumewuip/cmd">Github</Link>
       </Small>
 
       <script data-goatcounter="https://cmd-wuips.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
