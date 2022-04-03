d0d2432437b2a27c7f2b47d13fd7a89b12f1cd69
Guillaume Clochard <guillaume.clochard@blablacar.com>
2022-04-04 00:29:29 +0200

    update dependencies

--- blog/pages/api/health.tsx
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(_: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    message: "hello world",
-  });
-}
