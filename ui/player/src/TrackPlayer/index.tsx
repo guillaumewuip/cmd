638fd826f22f5d2f9f71b22d811358e0b27de768
Guillaume Clochard <guillaume.clochard@blablacar.com>
2022-04-08 13:54:27 +0200

    concentrate TrackPlayers

--- ui/player/src/TrackPlayer/index.tsx
+++ ui/player/src/TrackPlayer/index.tsx
@@ -1,17 +1,85 @@
 /* eslint-disable react/no-unstable-nested-components */
-import { useLayoutEffect, useMemo, useState } from "react";
+import { useLayoutEffect, useMemo, useState, useEffect, useRef } from "react";
 
 import * as Option from "fp-ts/Option";
 import { pipe } from "fp-ts/function";
 
-import { register, EmbedableLink } from "@cmd/domain-player";
+import {
+  register,
+  EmbedableLink,
+  loadBandcamp,
+  loadSoundcloud,
+  loadYoutube,
+} from "@cmd/domain-player";
 
-import * as Youtube from "./Youtube";
-import * as Soundcloud from "./Soundcloud";
-import * as Bandcamp from "./Bandcamp";
+import { VisuallyAndAriaHidden } from "../components/Hidden";
+import TrackBar from "./TrackBar";
 
 import { previousTitle } from "./previousTitle";
 import { position } from "./position";
+
+function Bandcamp({ id, href }: { id: string; href: string }) {
+  useEffect(() => {
+    loadBandcamp({
+      id,
+      bandcampUrl: href,
+    })();
+  }, [id, href]);
+
+  return <TrackBar id={id} />;
+}
+
+function Soundcloud({ id, href }: { id: string; href: string }) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (ref.current === null) {
+      throw new Error("Soundcloud ref is empty");
+    }
+
+    loadSoundcloud({
+      id,
+      soundcloudUrl: href,
+      container: ref.current,
+    })();
+  }, [id, href]);
+
+  return (
+    <>
+      <VisuallyAndAriaHidden>
+        <div ref={ref} tabIndex={-1} />
+      </VisuallyAndAriaHidden>
+
+      <TrackBar id={id} />
+    </>
+  );
+}
+
+function Youtube({ id, youtubeId }: { id: string; youtubeId: string }) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (ref.current === null) {
+      throw new Error("Youtube ref is empty");
+    }
+
+    loadYoutube({
+      id,
+      youtubeId,
+      container: ref.current,
+    })();
+  }, [id, youtubeId]);
+
+  return (
+    <>
+      <VisuallyAndAriaHidden>
+        <div ref={ref} tabIndex={-1} />
+      </VisuallyAndAriaHidden>
+
+      <TrackBar id={id} />
+    </>
+  );
+}
 
 export function TrackPlayer({
   embedableLink,
@@ -48,11 +116,9 @@ export function TrackPlayer({
         pipe(
           embedableLink,
           EmbedableLink.fold({
-            Youtube: ({ trackId }) => (
-              <Youtube.Player id={id} youtubeId={trackId} />
-            ),
-            Soundcloud: ({ href }) => <Soundcloud.Player id={id} href={href} />,
-            Bandcamp: ({ href }) => <Bandcamp.Player id={id} href={href} />,
+            Youtube: ({ trackId }) => <Youtube id={id} youtubeId={trackId} />,
+            Soundcloud: ({ href }) => <Soundcloud id={id} href={href} />,
+            Bandcamp: ({ href }) => <Bandcamp id={id} href={href} />,
           })
         )}
     </div>
