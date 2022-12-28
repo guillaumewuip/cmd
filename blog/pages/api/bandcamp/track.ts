1bc10d05ea824167f03484667e0757a8528c7034
Guillaume Clochard <guillaume.clochard@blablacar.com>
2022-12-28 19:27:48 +0100

    copy apis in blog

---
+++ blog/pages/api/bandcamp/track.ts
@@ -0,0 +1,98 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import fetch from "cross-fetch";
+
+import { parseHTML } from "linkedom";
+
+async function fetchPage(url: string): Promise<Document> {
+  const response = await fetch(url);
+  const documentString = await response.text();
+
+  return parseHTML(documentString).window.document;
+}
+
+function extractTrackId(document: Document): string {
+  const metaPagePropertiesElement = document.querySelector(
+    'meta[name="bc-page-properties"]'
+  );
+
+  if (!metaPagePropertiesElement) {
+    throw new Error(`Can't find bc-page-properties meta tag`);
+  }
+
+  const content = metaPagePropertiesElement.getAttribute("content");
+
+  if (!content) {
+    throw new Error(`No content on bc-page-properties meta tag`);
+  }
+
+  const pageProperties = JSON.parse(content);
+
+  if (!("item_id" in pageProperties)) {
+    throw new Error(`Can't find item_id in page properties`);
+  }
+
+  return pageProperties.item_id;
+}
+
+function extractStream(document: Document): string {
+  const script = document.querySelector("script[data-tralbum]");
+
+  if (!script) {
+    throw new Error(`Can't find data-tralbum script tag`);
+  }
+
+  const data = JSON.parse(script.getAttribute("data-tralbum") || "");
+
+  const url = Object.values(data.trackinfo[0].file)[0] as string;
+
+  return url;
+}
+
+function extractThumbnail(document: Document): string | null {
+  const link = document.querySelector('link[rel="image_src"]');
+
+  if (!link) {
+    throw new Error(`Can't find image_src link tag`);
+  }
+
+  const href = link.getAttribute("href");
+
+  return href;
+}
+
+export default async function handleBandcamp(
+  req: VercelRequest,
+  res: VercelResponse
+) {
+  try {
+    if (req.method !== "GET") {
+      res.status(400);
+      return;
+    }
+
+    const { url } = req.query;
+
+    if (!url || Array.isArray(url)) {
+      res.status(400).json({
+        error: "Missing correct url query parameter",
+      });
+      return;
+    }
+
+    const page = await fetchPage(decodeURIComponent(url));
+    const trackId = extractTrackId(page);
+    const streamUrl = extractStream(page);
+    const thumbnail = extractThumbnail(page);
+
+    res.status(200).json({
+      trackId,
+      streamUrl,
+      thumbnail,
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+
+    res.status(500);
+  }
+}
